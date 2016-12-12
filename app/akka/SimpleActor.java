925b86dc275366ee53aad4dea892246e242a4f62
Slawomir Leski <slawomir.leski@gmail.com>
2016-12-12 17:45:35 +0100

    Added simple akka actor.

---
+++ app/akka/SimpleActor.java
@@ -0,0 +1,13 @@
+package akka;
+
+import akka.actor.AbstractLoggingActor;
+
+/**
+ * Created by Slawomir Leski <slawomir.leski@idnow.de> on 12.12.16.
+ */
+public class SimpleActor extends AbstractLoggingActor {
+
+	public SimpleActor() {
+		super();
+	}
+}
