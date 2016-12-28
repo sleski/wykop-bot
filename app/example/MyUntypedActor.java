df9c4f5e1efc8ec8c537945488c1025e71c79e67
Slawomir Leski <slawomir.leski@gmail.com>
2016-12-28 14:03:08 +0100

    commit all changes

--- app/example/MyUntypedActor.java
+++ app/example/MyUntypedActor.java
@@ -3,12 +3,14 @@ package example;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import play.Configuration;
 
 import java.text.SimpleDateFormat;
 
 /**
  * Created by Slawomir Leski <slawomir.leski@gmail.com> on 25.12.16.
  */
+//TODO use extends AbstractLoggingActor instead of UntypedActor
 public class MyUntypedActor extends UntypedActor {
 
 	LoggingAdapter log = Logging.getLogger(getContext().system(), this);
@@ -18,6 +20,12 @@ public class MyUntypedActor extends UntypedActor {
 	public void onReceive(Object message) throws Exception {
 
 		log.info("Actor = {}, is running , {} .", ACTOR_NAME, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
+
+		System.out.println("Messages = " + message);
+
+		if(message instanceof Configuration){
+			System.out.println(((Configuration) message).getString("bot.key"));
+		}
 
 		if (message instanceof String) {
 			log.info("Received String message: {}", message);
