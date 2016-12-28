b1e0a41c8ed8d0e59963b1a6f13dfe2f00db3cca
Slawomir Leski <slawomir.leski@gmail.com>
2016-12-28 16:07:08 +0100

    Added new actor to impl.

---
+++ app/example/NewActor.java
@@ -0,0 +1,11 @@
+package example;
+
+import akka.actor.AbstractLoggingActor;
+
+/**
+ * Created by Slawomir Leski <slawomir.leski@idnow.de> on 28.12.16.
+ */
+public class NewActor extends AbstractLoggingActor{
+
+	//TODO implement this new Aactor
+}
