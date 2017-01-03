7435d54a7a225b4ac813fbc550d3e4eb17ce1fde
Slawomir Leski <slawomir.leski@gmail.com>
2017-01-03 11:37:54 +0100

    Added small todo list.

--- app/example/NewActor.java
+++ app/example/NewActor.java
@@ -5,7 +5,13 @@ import akka.actor.AbstractLoggingActor;
 /**
  * Created by Slawomir Leski <slawomir.leski@idnow.de> on 28.12.16.
  */
-public class NewActor extends AbstractLoggingActor{
+public class NewActor extends AbstractLoggingActor {
 
-	//TODO implement this new Aactor
+	//TODO implement this new Actor
+	/*
+	-create actor with some parameters
+	-call second actor
+	-wait for response (SUCCESS or FAILED)
+	-if FAILED repeat 5 times
+	 */
 }
